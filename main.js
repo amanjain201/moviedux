a05079db216be7ea1ff6374da46d3dd2025bb305
amanjain201@gmail.com <amanjain201@gmail.com>
2024-11-20 20:33:20 +0530

    Github page deployment support

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
-const  addTraditional = function (a,b) {
-    return a+b;
-}
+const addTraditional = function (a, b) {
+  return a + b;
+};
 
-const add = (a,b) => a + b;
+const add = (a, b) => a + b;
 
-const add2 = (a,b) => {
-    console.log("Starting sum");
-    return a+b;
+const add2 = (a, b) => {
+  console.log("Starting sum");
+  return a + b;
 };
 
 // Real world use
-const ids = [1,2,3,4,5];
+const ids = [1, 2, 3, 4, 5];
 
-ids.forEach(id => {
-    id++;
-    console.log(id);
+ids.forEach((id) => {
+  id++;
+  console.log(id);
 });
 
 // console.log(add2(1,2));
 
 // rest operator (ES6 feature)
 function testRest(firstArg, secondArg, ...otherArgs) {
-    console.log(firstArg);
-    console.log(secondArg);
-    console.log(otherArgs);
+  console.log(firstArg);
+  console.log(secondArg);
+  console.log(otherArgs);
 }
 
-testRest("Aman","Jain","Is","The","Best");
+testRest("Aman", "Jain", "Is", "The", "Best");
 
 // Spread operator ...  (used in react)
 const fruits = ["apple", "banana"];
 const moreFruits = ["cherry", "melon"];
 
-const allFruits = [...fruits, ...moreFruits];  // Merges 2 arraya
+const allFruits = [...fruits, ...moreFruits]; // Merges 2 arraya
 const freshFruits = [...fruits, "cherry"];
 
 console.log(fruits);
